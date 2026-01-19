91fcf92615e5d7aee70cfea18980de394db305ed
QuantumFF <jonathanczt@gmail.com>
2026-01-20 03:35:43 +0800

    chore: moved the db to the backend dir

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./wallpapers.db"
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_DIR = os.path.join(BASE_DIR, "db")
+
+# Ensure the db directory exists
+if not os.path.exists(DB_DIR):
+    os.makedirs(DB_DIR)
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(DB_DIR, 'wallpapers.db')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
